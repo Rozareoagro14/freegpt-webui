95093603d1daf5bffda3430aebb65345999409a6
Rozareoagro14 <150521745+Rozareoagro14@users.noreply.github.com>
2023-11-26 13:25:28 +0300

    Removed call to external API for searching results
    Removed call to external API for searching results.
    Removed the fetch_search_results(query) function.
    
    This code builds a conversation and adds unlock instructions (if provided) and the initial part of the conversation, but no longer contains the functionality to send queries to the external API to find results.

--- server/backend.py
+++ server/backend.py
@@ -1,125 +1,91 @@
-import re
-from datetime import datetime
-from g4f import ChatCompletion
-from flask import request, Response, stream_with_context
-from requests import get
-from server.config import special_instructions
-
+import re  # Импорт модуля для работы с регулярными выражениями
+from datetime import datetime  # Импорт класса datetime из модуля datetime
+from g4f import ChatCompletion  # Импорт класса ChatCompletion из модуля g4f
+from flask import request, Response, stream_with_context  # Импорт необходимых объектов из модуля Flask
+from requests import get  # Импорт функции get из модуля requests
+from server.config import special_instructions  # Импорт special_instructions из модуля server.config
 
 class Backend_Api:
     def __init__(self, bp, config: dict) -> None:
         """
-        Initialize the Backend_Api class.
-        :param app: Flask application instance
-        :param config: Configuration dictionary
+        Инициализация класса Backend_Api.
+        :param app: Экземпляр приложения Flask
+        :param config: Словарь конфигурации
         """
-        self.bp = bp
+        self.bp = bp  # Присваивание атрибута экземпляру класса
         self.routes = {
             '/backend-api/v2/conversation': {
-                'function': self._conversation,
-                'methods': ['POST']
+                'function': self._conversation,  # Метод, обрабатывающий маршрут '/backend-api/v2/conversation'
+                'methods': ['POST']  # Разрешенные методы запроса
             }
         }
 
     def _conversation(self):
         """  
-        Handles the conversation route.  
-
-        :return: Response object containing the generated conversation stream  
+        Обработка маршрута для беседы.  
+        :return: Объект Response, содержащий поток сгенерированной беседы  
         """
-        conversation_id = request.json['conversation_id']
+        conversation_id = request.json['conversation_id']  # Получение идентификатора беседы из запроса
 
         try:
-            jailbreak = request.json['jailbreak']
-            model = request.json['model']
-            messages = build_messages(jailbreak)
+            jailbreak = request.json['jailbreak']  # Получение инструкций о разблокировке из запроса
+            model = request.json['model']  # Получение параметров модели из запроса
+            messages = build_messages(jailbreak)  # Создание сообщений для беседы
 
-            # Generate response
+            # Генерация ответа
             response = ChatCompletion.create(
                 model=model,
                 chatId=conversation_id,
                 messages=messages
             )
 
+            # Возврат объекта Response с потоком данных в формате text/event-stream
             return Response(stream_with_context(generate_stream(response, jailbreak)), mimetype='text/event-stream')
 
-        except Exception as e:
-            print(e)
-            print(e.__traceback__.tb_next)
+        except Exception as e:  # Обработка исключений
+            print(e)  # Вывод информации об ошибке
+            print(e.__traceback__.tb_next)  # Вывод следующего трассировочного объекта
 
             return {
                 '_action': '_ask',
                 'success': False,
-                "error": f"an error occurred {str(e)}"
+                "error": f"Произошла ошибка: {str(e)}"  # Возврат информации об ошибке
             }, 400
 
 
 def build_messages(jailbreak):
     """  
-    Build the messages for the conversation.  
-
-    :param jailbreak: Jailbreak instruction string  
-    :return: List of messages for the conversation  
+    Составление сообщений для беседы без запроса к внешнему API для поиска.
+    :param jailbreak: Строка инструкций о разблокировке
+    :return: Список сообщений для беседы
     """
     _conversation = request.json['meta']['content']['conversation']
     internet_access = request.json['meta']['content']['internet_access']
     prompt = request.json['meta']['content']['parts'][0]
 
-    # Add the existing conversation
+    # Добавление существующей беседы
     conversation = _conversation
 
-    # Add web results if enabled
-    if internet_access:
-        current_date = datetime.now().strftime("%Y-%m-%d")
-        query = f'Current date: {current_date}. ' + prompt["content"]
-        search_results = fetch_search_results(query)
-        conversation.extend(search_results)
-
-    # Add jailbreak instructions if enabled
+    # Добавление инструкций о разблокировке, если они предоставлены
     if jailbreak_instructions := getJailbreak(jailbreak):
         conversation.extend(jailbreak_instructions)
 
-    # Add the prompt
+    # Добавление начальной части беседы
     conversation.append(prompt)
 
-    # Reduce conversation size to avoid API Token quantity error
+    # Уменьшение размера беседы для предотвращения ошибки количества токенов API
     if len(conversation) > 3:
         conversation = conversation[-4:]
 
     return conversation
 
 
-def fetch_search_results(query):
-    """  
-    Fetch search results for a given query.  
-
-    :param query: Search query string  
-    :return: List of search results  
-    """
-    search = get('https://ddg-api.herokuapp.com/search',
-                 params={
-                     'query': query,
-                     'limit': 3,
-                 })
-
-    snippets = ""
-    for index, result in enumerate(search.json()):
-        snippet = f'[{index + 1}] "{result["snippet"]}" URL:{result["link"]}.'
-        snippets += snippet
-
-    response = "Here are some updated web searches. Use this to improve user response:"
-    response += snippets
-
-    return [{'role': 'system', 'content': response}]
-
-
 def generate_stream(response, jailbreak):
     """
-    Generate the conversation stream.
-
-    :param response: Response object from ChatCompletion.create
-    :param jailbreak: Jailbreak instruction string
-    :return: Generator object yielding messages in the conversation
+    Генерация потока беседы.
+    :param response: Объект Response от ChatCompletion.create
+    :param jailbreak: Строка инструкций о разблокировке
+    :return: Объект-генератор, выдающий сообщения в беседе
     """
     if getJailbreak(jailbreak):
         response_jailbreak = ''
@@ -138,11 +104,12 @@ def generate_stream(response, jailbreak):
         yield from response
 
 
-def response_jailbroken_success(response: str) -> bool:
-    """Check if the response has been jailbroken.
+# Дополнительные функции проверки наличия инструкций о разблокировке
 
-    :param response: Response string
-    :return: Boolean indicating if the response has been jailbroken
+def response_jailbroken_success(response: str) -> bool:
+    """Проверка, была ли беседа разблокирована.
+    :param response: Строка ответа
+    :return: Булево значение, указывающее, была ли беседа разблокирована
     """
     act_match = re.search(r'ACT:', response, flags=re.DOTALL)
     return bool(act_match)
@@ -150,20 +117,18 @@ def response_jailbroken_success(response: str) -> bool:
 
 def response_jailbroken_failed(response):
     """
-    Check if the response has not been jailbroken.
-
-    :param response: Response string
-    :return: Boolean indicating if the response has not been jailbroken
+    Проверка, не была ли беседа разблокирована.
+    :param response: Строка ответа
+    :return: Булево значение, указывающее, не была ли беседа разблокирована
     """
     return False if len(response) < 4 else not (response.startswith("GPT:") or response.startswith("ACT:"))
 
 
 def getJailbreak(jailbreak):
     """  
-    Check if jailbreak instructions are provided.  
-
-    :param jailbreak: Jailbreak instruction string  
-    :return: Jailbreak instructions if provided, otherwise None  
+    Проверка наличия инструкций о разблокировке.  
+    :param jailbreak: Строка инструкций о разблокировке  
+    :return: Инструкции о разблокировке, если предоставлены, в противном случае None  
     """
     if jailbreak != "default":
         special_instructions[jailbreak][0]['content'] += special_instructions['two_responses_instruction']
@@ -174,3 +139,4 @@ def getJailbreak(jailbreak):
             return None
     else:
         return None
+
